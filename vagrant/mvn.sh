088e4b326599ea9c432c3ffb3621f97576d0d33c
Anand Raghunathan <anand.raghunathan@hotmail.com>
2018-06-17 10:50:53 -0700

    Vagrant file and other small changes

--- vagrant/mvn.sh
+++ vagrant/mvn.sh
@@ -1,5 +1,4 @@
 # Apache Maven Environment Variables
-# MAVEN_HOME for Maven 1 - M2_HOME for Maven 2
 export M2_HOME=/usr/local/apache-maven-3.5.3
-export M2=$M2_HOME/bin
-export PATH=$M2:$PATH
+export M2=/usr/local/apache-maven-3.5.3/bin
+export PATH=$PATH:/usr/local/apache-maven-3.5.3/bin
